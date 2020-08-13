2cf2848ee34e2f9cbb676bbe9ba246e46d1440b4
githubhair@gmail.com <C0mmun3!@>
2020-08-13 14:50:01 -0600

    Updates to WiFiTest

--- src/test/java/wi_fi/WiFiTest.java
+++ src/test/java/wi_fi/WiFiTest.java
@@ -1,11 +1,26 @@
 package wi_fi;
 
 import Base.BaseTests;
+import pages.HomePage;
+import org.testng.annotations.Test;
+import pages.WiFiPage;
 
 public class WiFiTest extends BaseTests {
 
-    public void clickWiFiLink(){
-        clickWiFiLink();
+    @Test
+    public void clickWiFiLink() throws InterruptedException {
+
+        HomePage homePage = new HomePage(driver);
+        homePage.onlyClickSettingsLink();
+        Thread.sleep(1000);
+
+       WiFiPage wiFiPage = new WiFiPage(driver);
+       wiFiPage.clickNetwork();
+       wiFiPage.clickWifiLink();
+
+       //wiFiPage.enterPassword();
+
+
     }
 
 }
